e3bdc1b6cc50efcbe6b11c17b516b33afbc3fd37
Brian Kuo <bjes940125@gmail.com>
2023-06-06 17:45:28 +0800

    2023.0606.1745

--- model/module/trans.py
+++ model/module/trans.py
@@ -587,28 +587,28 @@ class Transformer_Paper(nn.Module):
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  
 
         # Transformer Block with Attention @Paper
-        # self.blocks = nn.ModuleList([
-        #     Block(
-        #         dim=embed_dim, 
-        #         num_heads=h, 
-        #         mlp_hidden_dim=mlp_hidden_dim, 
-        #         qkv_bias=qkv_bias, 
-        #         qk_scale=qk_scale,
-        #         drop=drop_rate, 
-        #         attn_drop=attn_drop_rate, 
-        #         drop_path=dpr[i], 
-        #         norm_layer=norm_layer)
-        #     for i in range(depth)])
-        
-        # 2023.0517 ReLABlock @Brian
         self.blocks = nn.ModuleList([
-            ReLABlock(
+            Block(
                 dim=embed_dim, 
+                num_heads=h, 
                 mlp_hidden_dim=mlp_hidden_dim, 
+                qkv_bias=qkv_bias, 
+                qk_scale=qk_scale,
                 drop=drop_rate, 
+                attn_drop=attn_drop_rate, 
                 drop_path=dpr[i], 
                 norm_layer=norm_layer)
             for i in range(depth)])
+        
+        # 2023.0517 ReLABlock @Brian
+        # self.blocks = nn.ModuleList([
+        #     ReLABlock(
+        #         dim=embed_dim, 
+        #         mlp_hidden_dim=mlp_hidden_dim, 
+        #         drop=drop_rate, 
+        #         drop_path=dpr[i], 
+        #         norm_layer=norm_layer)
+        #     for i in range(depth)])
 
         # 2023.0513 MLPMixerBlock @Brian
         # self.blocks = nn.ModuleList([
